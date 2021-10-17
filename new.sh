d9ee67fd2162b9b01ddaa64e39fd01021d72ec9d
liusenmiao <16715443+liusenmiao@users.noreply.github.com>
2021-10-18 00:57:19 +0800

    Update new.sh

--- new.sh
+++ new.sh
@@ -81,7 +81,7 @@ echo -e "\nIf you continue, your disk will be formatted and no data will be pres
 confirm || exit -1
 
 echo "installing packages"
-pacman -Sy qemu-headless pv psmisc parted glibc
+pacman -Sy --noconfirm qemu-headless pv psmisc parted glibc
 
 echo "download image"
 wget ${ROUTEROS_URL} -O chr.img.zip
